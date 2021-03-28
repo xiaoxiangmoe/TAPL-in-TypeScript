30f8e67a9f149bdb4cfb5882efb4dde98d48d6b1
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2021-03-28 20:52:25 +0800

    build: update deps, add windows support

--- scripts/generate-peg.mjs
+++ scripts/generate-peg.mjs
@@ -1,22 +1,18 @@
-#!/usr/bin/env node
-
 // @ts-check
 
-import * as fs from 'node:fs';
-import * as path from 'node:path';
-import { execFileSync } from 'node:child_process';
+import { readdirSync } from 'node:fs';
+import { join } from 'node:path';
+import sh from 'shelljs';
 
 const dir = 'src/grammar';
 
 const pegFileExt = '.pegjs';
-fs.readdirSync(dir)
+
+readdirSync(dir)
   .filter(x => x.endsWith(pegFileExt))
   .forEach(fileName => {
     const targetFileName =
       fileName.substr(0, fileName.length - pegFileExt.length) + '-pegjs.ts';
 
-    execFileSync('node_modules/.bin/tspeg', [
-      path.join(dir, fileName),
-      path.join(dir, targetFileName),
-    ]);
+    sh.exec(`tspeg ${join(dir, fileName)} ${join(dir, targetFileName)}`);
   });
